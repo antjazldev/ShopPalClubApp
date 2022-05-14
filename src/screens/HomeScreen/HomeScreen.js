ced05dc1a0d90c5be8c55b711d5c49d6cba35ee8
antjazldev <antjazl@gmail.com>
2022-05-14 14:27:40 -0500

    Side menu component on HomeScreen

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,8 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {View,Text} from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import { UserContext } from '../../../context/userContext';
 import CustomWebView from '../../components/CustomWebView/CustomWebview';
+
+const Drawer = createDrawerNavigator();
 
 const HomeScreen = () =>{
 
@@ -10,7 +14,15 @@ const HomeScreen = () =>{
     const { user, login, logout } = React.useContext(UserContext);
 
     console.log('HOLA '+JSON.stringify(user));
-    return    (<CustomWebView url='https://google.com'/>
+    return    (<Drawer.Navigator>
+
+    <Drawer.Screen name="Home">
+      {props => <CustomWebView {...props}  url={"https://shoppalclub.com/htmlpage.html"} />}
+    </Drawer.Screen>
+    <Drawer.Screen name="Profile">
+      {props => <CustomWebView {...props}  url={"https://google.com"} />}
+    </Drawer.Screen>
+    </Drawer.Navigator>
 
     )
 }
